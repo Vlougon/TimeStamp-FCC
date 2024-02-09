5d0085c06f80558db3327b9b7dfe7776de0bd605
Victor <vlougongs@gmail.com>
2024-02-09 17:34:20 +0000

    Added /api/:date? endpoint Functionallity!

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,9 +21,29 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/:date?", function (req, res) {
+  const date_string = req.params.date;
+
+  if (date_string.match(/\d{5,}/)) {
+    const dateInt = parseInt(date_string);
+
+    res.json({ unix: dateInt, utc: new Date(dateInt).toUTCString() });
+  } else {
+    const dateObject = new Date(date_string);
+
+    if (dateObject.toString() === 'Invalid Date') {
+
+      res.json({ error: 'Invalid Date' });
+    } else {
+
+      res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+    }
+
+  }
+});
 
 
 // listen for requests :)
